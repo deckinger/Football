03f9eadd99432648e1646c982bd95a3bcab5c0cc
Adam Deckinger <deckinger@gmail.com>
2025-12-21 15:07:02 -0600

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Adam Deckinger on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
